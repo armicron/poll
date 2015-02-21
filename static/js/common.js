5b6b79fe4754733e0b408c122c7eb8f8ff27aeb3
Sergey Vishnikin <armicron@yandex.ru>
2015-02-21 12:15:32 +0300

    example chart

--- static/js/common.js
+++ static/js/common.js
@@ -1,1 +1,13 @@
- 
+requirejs.config({
+    baseUrl: "static/js",
+    paths: {
+        jquery: [
+            '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"',
+            'lib/jquery-2.1.3'
+        ],
+        underscore: 'lib/underscore',
+        backbone: 'lib/backbone',
+        b_tastypie: 'lib/backbone-tastypie',
+        chart: 'lib/Chart'
+    }
+});
