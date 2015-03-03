c30090c9e0705211670343fc331411a1ec9f30e9
Sergey Vishnikin <armicron@yandex.ru>
2015-03-03 15:03:39 +0300

    add X-CSRFToken header if token exists in a cookie

--- static/js/poll/components/AppModel.js
+++ static/js/poll/components/AppModel.js
@@ -3,17 +3,26 @@
  * http://paltman.com/integration-backbonejs-tastypie/
  */
 define([
-    'jquery', 'underscore', 'backbone'
+    'jquery', 'underscore', 'backbone', 'cookie'
 ],function($, _, $B) {
+    var oldSync = $B.sync;
     return $B.Model.extend({
         base_url: function() {
             var temp_url = Backbone.Model.prototype.url.call(this);
             return (temp_url.charAt(temp_url.length - 1) == '/' ? temp_url : temp_url+'/');
         },
-
         url: function() {
             return this.base_url();
+        },
+        sync: function(method, model, options) {
+            options.beforeSend = function(xhr) {
+                //var token = $('meta[name="csrf-token"]').attr('content');
+                var token = $.cookie('csrftoken');
+                if (token) {
+                    xhr.setRequestHeader('X-CSRFToken', token);
+                };
+            };
+            return oldSync(method, model, options);
         }
     });
-
 });
