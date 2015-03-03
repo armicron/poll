56556ab84426375529df238a407cf1096663148b
Sergey Vishnikin <armicron@yandex.ru>
2015-03-03 15:05:23 +0300

    user is able to vote through a js client

--- static/js/poll/views/PollView.js
+++ static/js/poll/views/PollView.js
@@ -1,12 +1,27 @@
 define([
-    'jquery', 'underscore', 'backbone', 'text!templates/poll_details.txt',
-], function($, _, $B, poll_details) {
+    'jquery', 'underscore', 'backbone', 'models/Vote',
+    'text!templates/poll_details.txt',
+], function($, _, $B, Vote, poll_details) {
     return $B.View.extend({
         template: _.template(poll_details),
-
+        events: {
+            'click input:button': 'vote',
+        },
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         },
+        vote: function(e) {
+            var vote = new Vote();
+            var $button = $(e.target);
+            var poll_id = $button.data('poll-id');
+            var choices = $button.parent().find('.poll_choice:checked');
+            $.each(choices, function (i, l) {
+                var choice_id = $(l).data('id');
+                vote.set({choice: '/polls/api/v1/choice/' + choice_id + '/',
+                          poll: '/polls/api/v1/poll/' + poll_id + '/'});
+                vote.save();
+            });
+        }
     });
 });
