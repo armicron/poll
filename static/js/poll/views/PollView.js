4fffa6ab1ec8d31e1123bc9924d9ca393e41218d
Sergey Vishnikin <armicron@yandex.ru>
2015-03-10 13:07:43 +0300

    create votes with one post request

--- static/js/poll/views/PollView.js
+++ static/js/poll/views/PollView.js
@@ -16,12 +16,20 @@ define([
             var $button = $(e.target);
             var poll_id = $button.data('poll-id');
             var choices = $button.parent().find('.poll_choice:checked');
-            $.each(choices, function (i, l) {
-                var choice_id = $(l).data('id');
-                vote.set({choice: '/polls/api/v1/choice/' + choice_id + '/',
-                          poll: '/polls/api/v1/poll/' + poll_id + '/'});
-                vote.save();
+            // $.each(choices, function (i, l) {
+            //     var choice_id = $(l).data('id');
+            //     vote.set({choice: '/polls/api/v1/choice/' + choice_id + '/',
+            //               poll: '/polls/api/v1/poll/' + poll_id + '/'});
+            //     vote.save();
+            // });
+            var choices_id = [];
+            $.each(choices, function(i, l) {
+                choices_id.push($(l).data('id'));
             });
+            vote.set({choice: choices_id,
+                      poll:'/polls/api/v1/poll/' + poll_id + '/'});
+            vote.save();
+            $button.parent().find('.poll_choices').fadeOut();
         }
     });
 });
